1f446b93c7a0ad7add824a82a835c06465d085f2
JLF <jlf-webdev@outlook.com>
2019-07-15 23:43:09 +0200

    Correcting touchscreen issues

--- website/color-picker/color-picker.js
+++ website/color-picker/color-picker.js
@@ -74,11 +74,11 @@ export class Picker {
         }
 
         // Register
-        this.target.addEventListener("mousedown", onMouseDown);
+        /*this.target.addEventListener("mousedown", onMouseDown);
         this.target.addEventListener("mousedown", () => this.onChangeCallback(this.getPickedColor()));
         this.target.addEventListener("mousemove", onMouseMove); 
         this.target.addEventListener("mousemove", () => this.onChangeCallback(this.getPickedColor()));
-        document.addEventListener("mouseup", onMouseUp);
+        document.addEventListener("mouseup", onMouseUp);*/
 
         this.target.addEventListener("touchstart", onMouseDown);
         this.target.addEventListener("touchstart", () => this.onChangeCallback(this.getPickedColor()));
